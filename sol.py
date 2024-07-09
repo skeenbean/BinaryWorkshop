08003e3efb5caabf7d99f646ef38247de7995080
skeenbean <mideolafusi@gmail.com>
2024-07-09 14:21:33 -0400

    updated sol.py with a random generator to be able to get the flag

--- sol.py
+++ sol.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
+
+from ctypes import CDLL
+libc = CDLL("libc.so.6")
+k = libc.time(0)
+libc.srand(k)
+#print("Current time is: %d" % k)
+'print(libc.rand())'
+w = libc.rand()
 
 from pwn import *
 
+io = process('sh')
+#io.sendline('echo Hello World')
 io = process('./time')
 print(io.recvuntil(b'Enter your number: '))
-io.sendline(b'765690')
-result  = io.recvline()
-print(result)
+io.sendline(b'%d' % w)
+#result  = io.recvline()
+#print(result)
+print(io.recvall())
